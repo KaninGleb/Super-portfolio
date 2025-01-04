85778584cf733fa85e397a684131523c542a358c
Kanin Gleb <gleb.kanin@gmail.com>
2025-01-04 17:45:27 +0300

    About-me started

---
+++ src/layout/sections/aboutMe/AboutMe.tsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+import {SectionTitle} from "../../../components/SectionTitle.tsx";
+import {Photo} from "../../../components/Photo.tsx";
+import aboutMePhoto from "../../../assets/images/about-me-photo.png";
+import {FlexWrapper} from "../../../components/FlexWrapper.tsx";
+
+export const AboutMe = () => {
+    return (
+        <StyledAboutMe>
+                <SectionTitle><span>#</span>about-me</SectionTitle>
+            <FlexWrapper>
+                <FlexWrapper>
+                    <Paragraph>Hello, i’m Elias! <br/> <br/>
+                        I’m a self-taught front-end developer based in Kyiv, Ukraine. I can develop responsive websites from
+                        scratch and raise them into modern user-friendly web experiences. <br/> <br/>
+                        Transforming my creativity and knowledge into a websites has been my passion for over a year. I have
+                        been helping various clients to establish their presence online. I always strive to learn about the
+                        newest technologies and frameworks.
+                    </Paragraph>
+                </FlexWrapper>
+
+                <Photo src={aboutMePhoto} width='339px' height='507px' alt=""/>
+            </FlexWrapper>
+        </StyledAboutMe>
+    );
+};
+
+const StyledAboutMe = styled.section`
+    background-color: #ff76ff;
+`
+
+const Paragraph = styled.p`
+    
+`
