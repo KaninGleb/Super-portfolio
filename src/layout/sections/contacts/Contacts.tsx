ba3694102d987c683f1e085420f6f056de9d2a21
Kanin Gleb <gleb.kanin@gmail.com>
2025-01-04 21:33:21 +0300

    Contacts added

--- src/layout/sections/contacts/Contacts.tsx
+++ src/layout/sections/contacts/Contacts.tsx
@@ -11,17 +11,68 @@ const text = {
 export const Contacts = () => {
     return (
         <StyledContacts>
-            <FlexWrapper>
-                <SectionTitle>contacts</SectionTitle>
-                <DecorativeLine width={'127px'} height={'2px'}/>
-            </FlexWrapper>
+                {/*<FlexWrapper>*/}
+                {/*    <SectionTitle>contacts</SectionTitle>*/}
+                {/*    <DecorativeLine width={'127px'} height={'2px'}/>*/}
+                {/*</FlexWrapper>*/}
 
-            <Paragraph children={text.contactsText}></Paragraph>
+                {/*<Paragraph children={text.contactsText}></Paragraph>*/}
+                <FlexWrapper>
+                    <SectionTitle>contacts</SectionTitle>
+                    <DecorativeLine width={'127px'} height={'2px'}/>
+                </FlexWrapper>
+
+            <FlexWrapper justify='space-between'>
+                <LeftSection>
+                    <Paragraph children={text.contactsText}></Paragraph>
+                </LeftSection>
+
+                <RightSection>
+                    <ContactInfo>
+                        <ContactItem>Message me here</ContactItem>
+                        <FlexWrapper direction='column'>
+                            <ContactItem>💬 Elias3519</ContactItem>
+                            <ContactItem>✉️ elias@elias.me</ContactItem>
+                        </FlexWrapper>
+                    </ContactInfo>
+                </RightSection>
+            </FlexWrapper>
         </StyledContacts>
     );
 };
 
-const StyledContacts = styled.div`
+const StyledContacts = styled.section`
     background-color: #942e4d;
     height: 228px;
+`
+
+const LeftSection = styled.div`
+    //flex: 1;
+    max-width: 505px;
+    width: 100%;
+    
+    border: 1px solid #00bfff;
+`
+
+const RightSection = styled.div`
+    display: flex;
+    gap: 16px;
+    
+    max-width: 204px;
+    width: 100%;
+    max-height: 141px;
+    height: 100%;
+    
+    padding: 16px;
+    
+    border: 1px solid #00bfff;
+`
+
+const ContactInfo = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
+const ContactItem = styled.p`
+
 `
