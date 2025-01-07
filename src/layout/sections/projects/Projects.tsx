96f505531c9a045dac6c72d0e243adaaa021551e
Kanin Gleb <gleb.kanin@gmail.com>
2025-01-07 23:24:28 +0300

    Projects styles improved

--- src/layout/sections/projects/Projects.tsx
+++ src/layout/sections/projects/Projects.tsx
@@ -7,7 +7,37 @@ import {Project} from "./project/Project.tsx";
 import chertNodes from '../../../assets/images/projects/project-1.svg'
 import protectX from '../../../assets/images/projects/project-2.svg'
 import kahoot from '../../../assets/images/projects/project-3.svg'
-import {theme} from "../../../styles/Theme.tsx";
+
+const projects = {
+    chertNodes: {
+        software: ['HTML', 'SCSS', 'Python', 'Flask'],
+        title: 'ChertNodes',
+        text: 'Minecraft servers hosting',
+        src: chertNodes,
+        links: [
+            { href: "#", children: "Live <~>", color: "#ffffff" },
+            { href: "#", children: "Cached >=", color: "#abb2bf", borderColor: "#abb2bf" },
+        ],
+    },
+    protectX: {
+        software: ['React', 'Express', 'Discord.js', 'Node.js', 'HTML', 'SCSS', 'Python', 'Flask'],
+        title: 'ProtectX',
+        text: 'Discord anti-crash bot',
+        src: protectX,
+        links: [
+            { href: "#", children: "Live <~>", color: "#ffffff" },
+        ],
+    },
+    kahoot: {
+        software: ['CSS', 'Express', 'Node.js'],
+        title: 'Kahoot Answers Viewer',
+        text: 'Get answers to your kahoot quiz',
+        src: kahoot,
+        links: [
+            { href: "#", children: "Live <~>", color: "#ffffff" },
+        ],
+    },
+};
 
 export const Projects = () => {
     return (
@@ -19,22 +49,17 @@ export const Projects = () => {
                     <button>View all ~~&gt;</button>
                 </FlexWrapper>
 
-                <FlexWrapper justify='space-around'>
-                    <Project software={'HTML SCSS Python Flask'}
-                                title={'ChertNodes'}
-                                text={'Minecraft servers hosting'}
-                                src={chertNodes}
-                                links={[theme.links.primaryLink, theme.links.secondaryLink]}/>
-                    <Project software={'React Express Discord.js Node.js HTML SCSS Python Flask'}
-                                title={'ProtectX'}
-                                text={'Discord anti-crash bot'}
-                                src={protectX}
-                                links={[theme.links.primaryLink]}/>
-                    <Project software={'CSS Express Node.js'}
-                                title={'Kahoot Answers Viewer'}
-                                text={'Get answers to your kahoot quiz'}
-                                src={kahoot}
-                                links={[theme.links.primaryLink]}/>
+                <FlexWrapper justify='space-between' gap='16px'>
+                    {Object.values(projects).map((project, index) => (
+                        <Project
+                            key={index}
+                            software={project.software}
+                            title={project.title}
+                            text={project.text}
+                            src={project.src}
+                            links={project.links}
+                        />
+                    ))}
                 </FlexWrapper>
             </Container>
         </StyledProjects>
--- src/layout/sections/projects/project/Project.tsx
+++ src/layout/sections/projects/project/Project.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import {Link} from "../../../../components/Link.tsx";
 import {FlexWrapper} from "../../../../components/FlexWrapper.tsx";
+import {theme} from "../../../../styles/Theme.tsx";
 
 type LinkPropsType = {
     href: string;
@@ -11,7 +12,7 @@ type LinkPropsType = {
 
 type ProjectPropsType = {
     src: string
-    software: string
+    software: string[]
     title: string
     text: string
     links: LinkPropsType[]
@@ -21,42 +22,72 @@ export const Project = (props: ProjectPropsType) => {
     return (
         <StyledWork>
             <Image src={props.src} alt=""/>
-            <Software>{props.software}</Software>
-            <Title>{props.title}</Title>
-            <Text>{props.text}</Text>
-            <FlexWrapper gap="16px">
-                {props.links.map((link, index) => (
-                    <Link
-                        key={index}
-                        href={link.href}
-                        children={link.children}
-                        color={link.color}
-                        borderColor={link.borderColor}
-                    />
-                ))}
-            </FlexWrapper>
+            <SizeWrapper>
+                <Software>
+                    {props.software.map((software, index) => (
+                        <li key={index}>{software}</li>
+                    ))}
+                </Software>
+                <StyledHr/>
+                <MainSizeWrapper direction="column" gap='16px'>
+                    <Title>{props.title}</Title>
+                    <Text>{props.text}</Text>
+                    <FlexWrapper gap="16px">
+                        {props.links.map((link, index) => (
+                            <Link
+                                key={index}
+                                href={link.href}
+                                children={link.children}
+                                color={link.color}
+                                borderColor={link.borderColor}
+                            />
+                        ))}
+                    </FlexWrapper>
+                </MainSizeWrapper>
+            </SizeWrapper>
         </StyledWork>
     );
 };
 
 const StyledWork = styled.div`
+    border: 1px solid ${theme.colors.primaryBorder};
     max-width: 330px;
     width: 100%;
+    height: 100%;
 `
 
 const Image = styled.img`
     width: 100%;
     height: 201px;
+    border-bottom: 1px solid ${theme.colors.primaryBorder};
 `
 
-const Software = styled.span`
-    
+const SizeWrapper = styled.div`
+    padding: 8px;
+`
+
+const MainSizeWrapper = styled(FlexWrapper)`
+    margin: 16px 8px 8px;
+`
+
+const Software = styled.ul`
+    display: flex;
+    flex-wrap: wrap;
+    gap: 8px;
+`
+
+const StyledHr = styled.hr`
+    border: none;
+    height: 1px;
+    background-color: #abb2bf;
+    margin: 8px -8px;
 `
 
 const Title = styled.h3`
-    
+    font-weight: 500;
+    font-size: 24px;
 `
 
 const Text = styled.span`
-    
+    color: ${theme.colors.primaryLightText};
 `
