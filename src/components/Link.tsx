ee62aac85481f87983918ec9db427eb8e5fa4f7e
Kanin Gleb <gleb.kanin@gmail.com>
2025-01-08 19:09:19 +0300

    Link component styles supplemented

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,29 +1,28 @@
 import styled from "styled-components";
+import {theme} from "../styles/Theme.tsx";
 
 type LinkPropsType = {
     href: string
     color?: string
     children: string
     borderColor?: string
+    mWidth?: string
 }
 
 export const Link = (props: LinkPropsType) => {
     return (
-        <StyledLink href={props.href} color={props.color} borderColor={props.borderColor}>
+        <StyledLink href={props.href} color={props.color} borderColor={props.borderColor} mWidth={props.mWidth}>
             {props.children}
         </StyledLink>
     );
 };
 
 const StyledLink = styled.a<LinkPropsType>`
-    font-family: "Fira Code", sans-serif;
     font-weight: 500;
-    font-size: 16px;
-    text-decoration: none;
-    color: ${props => props.color || '#ffffff'};
+    color: ${props => props.color || theme.colors.primaryText};
     
-    border: 1px solid ${props => props.borderColor || '#c778dd'};
+    border: 1px solid ${props => props.borderColor || theme.colors.secondaryText};
     padding: 8px 16px;
-    max-width: 121px;
+    max-width: ${props => props.mWidth || '121px'};
     height: 37px;
 `
