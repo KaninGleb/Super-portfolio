ecd97bdfec5d91e6adf32f399b908ba95b3a060f
Kanin Gleb <gleb.kanin@gmail.com>
2025-01-08 16:33:04 +0300

    Header lang change styles added

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -3,6 +3,8 @@ import {Logo} from "../../components/logo/Logo.tsx";
 import {Menu} from "../../components/menu/Menu.tsx";
 import {FlexWrapper} from "../../components/FlexWrapper.tsx";
 import {Container} from "../../components/Container.tsx";
+import {theme} from "../../styles/Theme.tsx";
+import arrowClose from "../../assets/images/Lang-change-closed.svg";
 
 const headerLinks = ['home', 'works', 'about-me', 'contacts'];
 
@@ -15,11 +17,9 @@ export const Header = () => {
                     <FlexWrapper gap='32px'>
                         <Menu menuItems={headerLinks}/>
                         <LanguageSelect>
-                            <select>
-                                <option>EN</option>
-                                <option>RU</option>
-                                <option>UA</option>
-                            </select>
+                            <option>EN</option>
+                            <option>RU</option>
+                            <option>UA</option>
                         </LanguageSelect>
                     </FlexWrapper>
                 </FlexWrapper>
@@ -30,16 +30,37 @@ export const Header = () => {
 
 const StyledHeader = styled.header`
     padding: 32px 0 8px;
-    background-color: #bde0fe;
 `;
 
-const LanguageSelect = styled.div`
-    select {
-        border: none;
-        background-color: transparent;
+const LanguageSelect = styled.select`
+    font-family: "Fira Code", sans-serif;
+    font-weight: 600;
+    font-size: 16px;
+    
+    border: none;
+    background-color: transparent;
+    color: ${theme.colors.primaryLightText};
+    
+    appearance: none;
+    background-image: url(${arrowClose});
+    background-repeat: no-repeat;
+    background-position: right 10px center;
+    background-size: 10px;
+    
+    &:hover {
+        color: ${theme.colors.primaryText};
+    }
 
-        &:focus {
-            outline: none;
-        }
+    option {
+        color: ${theme.colors.primaryLightText};
+    }
+
+    &:hover option {
+        color: ${theme.colors.primaryLightText};
+    }
+
+    &:focus-visible {
+        outline: none;
+        background-color: ${theme.colors.primaryBg};
     }
 `;
