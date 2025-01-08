851d730d2e4f59eb5b4da479c822d30977205d1a
Kanin Gleb <gleb.kanin@gmail.com>
2025-01-08 22:11:41 +0300

    AboutMe component - styles improved

--- src/components/Paragraph.tsx
+++ src/components/Paragraph.tsx
@@ -1,16 +1,21 @@
-import React from "react";
 import styled from "styled-components";
+import {theme} from "../styles/Theme.tsx";
 
 type ParagraphPropsType = {
-    children: React.ReactNode;
+    children: string
 }
 
 export const Paragraph = (props: ParagraphPropsType) => {
     return (
-        <StyledParagraph>{props.children}</StyledParagraph>
+        <StyledParagraph>
+            {props.children.split('\n').map((line, index) => (
+                <span key={index}>{line}<br/></span>
+            ))}
+        </StyledParagraph>
     );
 };
 
 const StyledParagraph = styled.p`
-    
+    line-height: 1.625;
+    color: ${theme.colors.primaryLightText};
 `
--- src/layout/sections/aboutMe/AboutMe.tsx
+++ src/layout/sections/aboutMe/AboutMe.tsx
@@ -1,37 +1,37 @@
 import styled from "styled-components";
+import {theme} from "../../../styles/Theme.tsx";
 import {Container} from "../../../components/Container.tsx";
 import {FlexWrapper} from "../../../components/FlexWrapper.tsx";
 import {SectionTitle} from "../../../components/SectionTitle.tsx";
 import {DecorativeLine} from "../../../components/DecorativeLine.tsx";
 import {Paragraph} from "../../../components/Paragraph.tsx";
+import {Link} from "../../../components/Link.tsx";
 import {Photo} from "../../../components/Photo.tsx";
 import aboutMePhoto from "../../../assets/images/about-me-photo.png";
-import {theme} from "../../../styles/Theme.tsx";
 
 const text = {
-    aboutMeText: 'Hello, i’m Elias! ' +
-        'I’m a self-taught front-end developer based in Kyiv, Ukraine. I can develop responsive websites from scratch and raise them into modern user-friendly web experiences. ' +
-        'Transforming my creativity and knowledge into a websites has been my passion for over a year. I have been helping various clients to establish their presence online. I always strive to learn about the newest technologies and frameworks.',
+    aboutMeText: 'Hello, I’m Elias!\n\n' +
+        'I’m a self-taught front-end developer based in Kyiv, Ukraine. I can develop responsive websites from scratch and raise them into modern user-friendly web experiences.\n\n' +
+        'Transforming my creativity and knowledge into websites has been my passion for over a year. I have been helping various clients to establish their presence online. I always strive to learn about the newest technologies and frameworks.',
 }
 
 export const AboutMe = () => {
     return (
         <StyledAboutMe>
             <Container>
-                <FlexWrapper>
-                    <SectionTitle><span>#</span>about-me</SectionTitle>
-                    <DecorativeLine width={'326px'} height={'2px'}/>
-                </FlexWrapper>
-
-                <FlexWrapper>
-                    <FlexWrapper>
+                <FlexWrapper gap='166px'>
+                    <FlexWrapper direction='column' gap='27px'>
+                        <StyledSectionTitle>
+                            <SectionTitle><span>#</span>about-me</SectionTitle>
+                            <DecorativeLine width={'326px'} height={'2px'}/>
+                        </StyledSectionTitle>
                         <Paragraph children={text.aboutMeText}/>
+                        <Link href={'#'} children={'Read more ->'} mWidth={'150px'}/>
                     </FlexWrapper>
                     <FlexWrapper direction={'column'}>
                         <Photo src={aboutMePhoto} width='339px' height='507px' alt=""/>
                         <StyledBorder/>
                     </FlexWrapper>
-
                 </FlexWrapper>
             </Container>
         </StyledAboutMe>
@@ -39,7 +39,14 @@ export const AboutMe = () => {
 };
 
 const StyledAboutMe = styled.section`
-    background-color: #425293;
+    max-height: 508px;
+    height: 100%;
+    margin-bottom: 112px;
+`
+
+const StyledSectionTitle = styled(FlexWrapper)`
+    width: 516px;
+    margin-bottom: 4px;
 `
 
 const StyledBorder = styled.div`
