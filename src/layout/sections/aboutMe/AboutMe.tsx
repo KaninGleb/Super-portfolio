8828e3d3fd11cbb1ed56d5c2d06b525cd883a2cd
Kanin Gleb <gleb.kanin@gmail.com>
2025-01-08 20:37:08 +0300

    AbouyMe component styles improved

--- src/layout/sections/aboutMe/AboutMe.tsx
+++ src/layout/sections/aboutMe/AboutMe.tsx
@@ -6,6 +6,7 @@ import {DecorativeLine} from "../../../components/DecorativeLine.tsx";
 import {Paragraph} from "../../../components/Paragraph.tsx";
 import {Photo} from "../../../components/Photo.tsx";
 import aboutMePhoto from "../../../assets/images/about-me-photo.png";
+import {theme} from "../../../styles/Theme.tsx";
 
 const text = {
     aboutMeText: 'Hello, i’m Elias! ' +
@@ -26,7 +27,11 @@ export const AboutMe = () => {
                     <FlexWrapper>
                         <Paragraph children={text.aboutMeText}/>
                     </FlexWrapper>
-                    <Photo src={aboutMePhoto} width='339px' height='507px' alt=""/>
+                    <FlexWrapper direction={'column'}>
+                        <Photo src={aboutMePhoto} width='339px' height='507px' alt=""/>
+                        <StyledBorder/>
+                    </FlexWrapper>
+
                 </FlexWrapper>
             </Container>
         </StyledAboutMe>
@@ -36,3 +41,10 @@ export const AboutMe = () => {
 const StyledAboutMe = styled.section`
     background-color: #425293;
 `
+
+const StyledBorder = styled.div`
+    width: 271px;
+    height: 1px;
+    background-color: ${theme.colors.secondaryBorder};
+    margin-left: 45px;
+`;
