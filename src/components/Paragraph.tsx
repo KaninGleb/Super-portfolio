a90873ede4443008933dd2cfe3e324e08433e1d4
Kanin Gleb <gleb.kanin@gmail.com>
2025-01-08 23:24:06 +0300

    Paragraph fontWidth props added

--- src/components/Paragraph.tsx
+++ src/components/Paragraph.tsx
@@ -3,19 +3,21 @@ import {theme} from "../styles/Theme.tsx";
 
 type ParagraphPropsType = {
     children: string
+    fontWidth?: string
 }
 
 export const Paragraph = (props: ParagraphPropsType) => {
     return (
-        <StyledParagraph>
-            {props.children.split('\n').map((line, index) => (
-                <span key={index}>{line}<br/></span>
+        <StyledParagraph fontWidth={props.fontWidth}>
+            {props.children.split('\n').map((text, index) => (
+                <span key={index}>{text}<br/></span>
             ))}
         </StyledParagraph>
     );
 };
 
-const StyledParagraph = styled.p`
+const StyledParagraph = styled.p<{fontWidth?: string}>`
     line-height: 1.625;
+    font-weight: ${props => props.fontWidth || '400'};
     color: ${theme.colors.primaryLightText};
 `
