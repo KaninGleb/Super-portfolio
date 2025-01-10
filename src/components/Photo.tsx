274f9c661b9dc02e6c2164b7be79e91c9914b67b
Kanin Gleb <gleb.kanin@gmail.com>
2025-01-10 15:29:39 +0300

    Photo refactor

--- src/components/Photo.tsx
+++ src/components/Photo.tsx
@@ -9,6 +9,5 @@ export const Photo = styled.img<PhotoPropsType>`
     max-width: ${props => props.width}
     max-height: ${props => props.height}
     width: 100%;
-    height: 100%;
     object-fit: cover;
 `
