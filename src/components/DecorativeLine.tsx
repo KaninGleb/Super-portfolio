d077869505db2f0e54e816cea9a759a8de3f873f
Kanin Gleb <gleb.kanin@gmail.com>
2025-01-12 22:22:32 +0300

    DecorativeLine props supplemented

--- src/components/DecorativeLine.tsx
+++ src/components/DecorativeLine.tsx
@@ -1,25 +1,28 @@
 import styled from "styled-components";
+import {theme} from "../styles/Theme.tsx";
 
 type DecorativeLinePropsType = {
     width: string
     height: string
+    bgColor?: string
+    marginL?: string
 }
 
 export const DecorativeLine = (props: DecorativeLinePropsType) => {
     return (
-        <LineContainer width={props.width} height={props.height}/>
+        <LineContainer width={props.width} height={props.height} bgColor={props.bgColor} marginL={props.marginL}/>
     );
 };
 
 const LineContainer = styled.div<DecorativeLinePropsType>`
     display: flex;
     align-items: center;
-    margin-left: 16px;
+    margin-left: ${props => props.marginL || '16px'};
     
     &:after {
         content: '';
         width: ${props => props.width};
         height: ${props => props.height};
-        background-color: #c778dd;
+        background-color: ${props => props.bgColor || theme.colors.secondaryOutline};
     }
 `;
