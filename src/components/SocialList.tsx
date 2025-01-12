63b3eaf2960d939813a0fe2fbb9d8b9ce9b1aa05
Kanin Gleb <gleb.kanin@gmail.com>
2025-01-12 19:42:41 +0300

    iconsTitles props for SocialList added

--- src/components/SocialList.tsx
+++ src/components/SocialList.tsx
@@ -1,15 +1,18 @@
 import styled from "styled-components";
+import {theme} from "../styles/Theme.tsx";
 import {Icon} from "./icon/Icon.tsx";
 
 export const socialData = [
-    { id: 'git', href: "#" },
-    { id: 'figma', href: "#" },
-    { id: 'discord', href: "#" },
+    { id: 'git', href: "https://github.com/" },
+    { id: 'figma', href: "https://www.figma.com/" },
+    { id: 'discord', href: "https://discord.com/" },
+    { id: 'email', href: "https://mail.google.com/" },
 ]
 
 type SocialListPropsType = {
-    iconIds: string[];
-    iconSize?: string;
+    iconIds: string[]
+    iconsTitles?: string[]
+    iconSize?: string
     display?: boolean
     direction?: string
     gap?: string
@@ -27,8 +30,9 @@ export const SocialList = (props: SocialListPropsType) => {
                             direction={props.direction}
                             gap={props.gap}
         >
-            {props.iconIds.map(itemId => {
+            {props.iconIds.map((itemId, index) => {
                 const socialItem = socialData.find(item => item.id === itemId);
+                const title = props.iconsTitles ? props.iconsTitles[index] : ''
 
                 return socialItem && (
                     <SocialItem key={itemId}>
@@ -38,8 +42,8 @@ export const SocialList = (props: SocialListPropsType) => {
                                     height={props.iconSize || '32'}
                                     viewBox={`0 0 32 32`}
                                     // viewBox={`0 0 ${props.iconSize || '32'} ${props.iconSize || '32'}`}
-
                             />
+                            {title && <IconTitle>{title}</IconTitle>}
                         </SocialLink>
                     </SocialItem>
                 )
@@ -55,9 +59,29 @@ const StyledSocialList = styled.ul<FlexPropsType>`
 `
 
 const SocialItem = styled.li`
-
+    
 `
 
 const SocialLink = styled.a`
-    display: inline-block;
+    display: inline-flex;
+    align-items: center;
+    gap: 5px;
+    
+    transition: 0.2s ease;
+    &:hover {
+        transform: translateY(-3px);
+        color: ${theme.colors.primaryText};
+        span {
+            color: ${theme.colors.primaryText};
+        }
+    }
+`
+
+const IconTitle = styled.span`
+    display: flex;
+    align-items: center;
+    font-family: "Fira Code", sans-serif;
+    font-weight: 400;
+    font-size: 16px;
+    color: ${theme.colors.primaryLightText};
 `
