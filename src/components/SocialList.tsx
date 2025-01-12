ece3ff2959c8f47fcf973e11f62babd5bb75419d
Kanin Gleb <gleb.kanin@gmail.com>
2025-01-12 20:22:17 +0300

    SocialList aria labels added

--- src/components/SocialList.tsx
+++ src/components/SocialList.tsx
@@ -3,10 +3,10 @@ import {theme} from "../styles/Theme.tsx";
 import {Icon} from "./icon/Icon.tsx";
 
 export const socialData = [
-    { id: 'git', href: "https://github.com/" },
-    { id: 'figma', href: "https://www.figma.com/" },
-    { id: 'discord', href: "https://discord.com/" },
-    { id: 'email', href: "https://mail.google.com/" },
+    { id: 'git', href: "https://github.com/", areaLabel: 'Link to GitHub'},
+    { id: 'figma', href: "https://www.figma.com/", areaLabel: 'Link to Figma'},
+    { id: 'discord', href: "https://discord.com/", areaLabel: 'Link to Discord'},
+    { id: 'email', href: "https://mail.google.com/", areaLabel: 'Link to Gmail'},
 ]
 
 type SocialListPropsType = {
@@ -37,16 +37,18 @@ export const SocialList = (props: SocialListPropsType) => {
 
                 return socialItem && (
                     <SocialItem key={itemId}>
-                        <SocialLink href={socialItem.href} target='_blank'>
+                        <SocialLink href={socialItem.href}
+                                    target='_blank'
+                                    aria-label={socialItem.areaLabel}>
                             {props.showIcon && (
-                            <Icon iconId={socialItem.id}
-                                    width={props.iconSize || '32'}
-                                    height={props.iconSize || '32'}
-                                    viewBox={`0 0 32 32`}
-                                    // viewBox={`0 0 ${props.iconSize || '32'} ${props.iconSize || '32'}`}
-                            />
+                                <Icon iconId={socialItem.id}
+                                        width={props.iconSize || '32'}
+                                        height={props.iconSize || '32'}
+                                        viewBox={`0 0 32 32`}
+                                        // viewBox={`0 0 ${props.iconSize || '32'} ${props.iconSize || '32'}`}
+                                />
                             )}
-                            {title && <IconTitle>{title}</IconTitle>}
+                            <IconTitle>{title}</IconTitle>
                         </SocialLink>
                     </SocialItem>
                 )
