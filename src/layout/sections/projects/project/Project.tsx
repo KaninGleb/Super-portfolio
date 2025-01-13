65bc28f9ce7e43b07f3aa19bbadbf6de758f638a
Kanin Gleb <gleb.kanin@gmail.com>
2025-01-13 20:40:29 +0300

    Project - animation for images added

--- src/layout/sections/projects/project/Project.tsx
+++ src/layout/sections/projects/project/Project.tsx
@@ -22,7 +22,10 @@ type ProjectPropsType = {
 export const Project = (props: ProjectPropsType) => {
     return (
         <StyledWork>
-            <Image src={props.src} alt=""/>
+            <StyledImageWrapper>
+                <Image src={props.src} alt=""/>
+                <Overlay/>
+            </StyledImageWrapper>
             <SizeWrapper>
                 <Software>
                     {props.software.map((software, index) => (
@@ -51,16 +54,69 @@ export const Project = (props: ProjectPropsType) => {
     );
 };
 
+const Overlay = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    opacity: 0;
+    transition: opacity 0.3s ease;
+
+    animation: gradientAnimation 3s infinite;
+
+    @keyframes gradientAnimation {
+        0% {
+            background: rgba(199, 120, 221, 0.5);
+            box-shadow:
+                    0 -150px 70px -120px rgba(171, 178, 191, 0.5) inset,
+                    0 -220px 70px -120px rgba(199, 120, 221, 0.3) inset,
+                    0 -280px 70px -120px rgba(171, 178, 191, 0.3) inset;
+        }
+        50% {
+            background: rgba(128, 0, 128, 0.5);
+            box-shadow:
+                    0 -140px 70px -120px rgba(186, 85, 211, 0.5) inset,
+                    0 -210px 70px -120px rgba(0, 128, 128, 0.5) inset,
+                    0 -280px 70px -120px rgba(128, 0, 128, 0.3) inset;
+        }
+        100% {
+            background: rgba(199, 120, 221, 0.5);
+            box-shadow:
+                    0 -150px 70px -120px rgba(171, 178, 191, 0.5) inset,
+                    0 -220px 70px -120px rgba(199, 120, 221, 0.3) inset,
+                    0 -280px 70px -120px rgba(171, 178, 191, 0.3) inset;
+        }
+    }
+`
+
 const StyledWork = styled.div`
     outline: 1px solid ${theme.colors.primaryOutline};
     max-width: 330px;
     width: 100%;
     height: 100%;
+    transition: transform 0.3s, box-shadow 0.3s, background-color 0.3s;
+
+    &:hover {
+        transform: scale(1.02);
+        box-shadow: 0 4px 20px rgba(0, 0, 0, 0.2);
+        background-color: rgba(40, 44, 51, 0.1);
+
+        ${Overlay} {
+            opacity: 1;
+        }
+    }
+`
+
+const StyledImageWrapper = styled.div`
+    width: 100%;
+    height: 201px;
+    position: relative;
 `
 
 const Image = styled.img`
     width: 100%;
-    height: 201px;
+    height: 100%;
     border-bottom: 1px solid ${theme.colors.primaryOutline};
     object-fit: cover;
 `
