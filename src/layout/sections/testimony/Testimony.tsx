8013a9cc7df4e12e5925e67a7f9014730542b5e6
Kanin Gleb <gleb.kanin@gmail.com>
2025-01-17 14:00:43 +0300

    Testimony quotation marks fixing, common font added

--- src/layout/sections/testimony/Testimony.tsx
+++ src/layout/sections/testimony/Testimony.tsx
@@ -3,18 +3,19 @@ import {theme} from "../../../styles/Theme.tsx";
 import {Container} from "../../../components/Container.tsx";
 import {FlexWrapper} from "../../../components/FlexWrapper.tsx";
 import quotationMark from "../../../assets/images/quotation-mark.svg"
+import {font, FontPropsType} from "../../../styles/CommonFont.tsx";
 
 export const Testimony = () => {
     return (
         <StyledTestimony>
             <Container>
                 <FlexWrapper justify="center">
-                    <QuotationMarkBefore src={quotationMark} alt="Quotation mark before"/>
-                    <FlexWrapper direction="column" align="flex-end">
-                        <StyledQuote>With great power comes great electricity bill</StyledQuote>
-                        <StyledCite>- Dr. Who</StyledCite>
-                    </FlexWrapper>
-                    <QuotationMarkAfter src={quotationMark} alt="Quotation mark after"/>
+                    <StyledFlexWrapper direction="column" align="flex-end">
+                        <QuotationMarkBefore src={quotationMark} alt="Quotation mark before"/>
+                        <StyledQuote weight={500} Fmax={24} Fmin={10}>With great power comes great electricity bill</StyledQuote>
+                        <StyledCite weight={500} Fmax={24} Fmin={10}>- Dr. Who</StyledCite>
+                        <QuotationMarkAfter src={quotationMark} alt="Quotation mark after"/>
+                    </StyledFlexWrapper>
                 </FlexWrapper>
             </Container>
         </StyledTestimony>
@@ -25,35 +26,49 @@ const StyledTestimony = styled.section`
     font-size: 24px;
     margin-bottom: 74px;
 `
+const StyledFlexWrapper = styled(FlexWrapper)`
+    position: relative;
+`
 
 const QuotationMarkBefore = styled.img`
-    position: relative;
+    position: absolute;
     top: -14px;
-    left: 50px;
+    left: 10px;
     max-height: 30px;
 `
 
-const StyledQuote = styled.blockquote`
-    font-weight: 500;
-
-    outline: 1px solid ${theme.colors.primaryOutline};
+const StyledQuote = styled.blockquote<FontPropsType>`
+    ${font};
+    border: 1px solid ${theme.colors.primaryOutline};
     padding: 32px;
     max-width: 714px;
     width: 100%;
-    max-height: 95px;
-    height: 100%;
-    position: relative;
-    z-index: -1;
+    //position: relative;
+    //z-index: -1;
+    
+    //&::before {
+    //    content: '';
+    //    background-image: url('../../../assets/images/quotation-mark.svg');
+    //    background-size: contain;
+    //    background-repeat: no-repeat;
+    //    position: absolute;
+    //    top: 10px;
+    //    left: 0px;
+    //    width: 30px;
+    //    height: 30px;
+    //    z-index: 1;
+    //}
 `
 
 const QuotationMarkAfter = styled.img`
-    position: relative;
+    position: absolute;
     top: 80px;
-    right: 60px;
+    right: 12px;
     max-height: 30px;
 `
 
-const StyledCite = styled.cite`
+const StyledCite = styled.cite<FontPropsType>`
+    ${font};
     border: 1px solid ${theme.colors.primaryOutline};
     border-top: none;
     padding: 16px;
