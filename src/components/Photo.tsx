13490bf319edb5115d72717a0318d6cbe7359dd2
Kanin Gleb <gleb.kanin@gmail.com>
2025-01-17 21:14:48 +0300

    Responsive width added

--- src/components/Photo.tsx
+++ src/components/Photo.tsx
@@ -2,12 +2,14 @@ import styled from "styled-components";
 
 type PhotoPropsType = {
     width: string
-    height: string
 }
 
 export const Photo = styled.img<PhotoPropsType>`
     display: block;
     width: ${props => props.width};
-    height: ${props => props.height};
     object-fit: cover;
 `
+
+// {width: clamp(${props => props.Wmin}px,
+//     calc((100vw - 375px) / (1024 - 375) * (${props => props.Wmax} - ${props => props.Wmin}) + ${props => props.Wmin}px),
+//     ${props => props.Wmax}px);
