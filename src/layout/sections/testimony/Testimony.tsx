fb2e627421c7b3606c0b664ec9a47508e354714f
Kanin Gleb <gleb.kanin@gmail.com>
2025-01-18 16:38:06 +0300

    Testimony quotation marks set by pseudo-elements

--- src/layout/sections/testimony/Testimony.tsx
+++ src/layout/sections/testimony/Testimony.tsx
@@ -11,10 +11,10 @@ export const Testimony = () => {
             <Container>
                 <FlexWrapper justify="center">
                     <StyledFlexWrapper direction="column" align="flex-end">
-                        <QuotationMarkBefore src={quotationMark} alt="Quotation mark before"/>
+                        {/*<QuotationMarkBefore src={quotationMark} alt="Quotation mark before"/>*/}
                         <StyledQuote>With great power comes great electricity bill</StyledQuote>
                         <StyledCite>- Dr. Who</StyledCite>
-                        <QuotationMarkAfter src={quotationMark} alt="Quotation mark after"/>
+                        {/*<QuotationMarkAfter src={quotationMark} alt="Quotation mark after"/>*/}
                     </StyledFlexWrapper>
                 </FlexWrapper>
             </Container>
@@ -30,12 +30,12 @@ const StyledFlexWrapper = styled(FlexWrapper)`
     position: relative;
 `
 
-const QuotationMarkBefore = styled.img`
-    position: absolute;
-    top: -14px;
-    left: 10px;
-    max-height: 30px;
-`
+// const QuotationMarkBefore = styled.img`
+//     position: absolute;
+//     top: -14px;
+//     left: 10px;
+//     max-height: 30px;
+// `
 
 const StyledQuote = styled.blockquote`
     ${font({weight:500, Fmax: 24, Fmin:16})};
@@ -46,30 +46,34 @@ const StyledQuote = styled.blockquote`
     //position: relative;
     //z-index: -1;
     
-    //&::before {
-    //    content: '';
-    //    background-image: url('../../../assets/images/quotation-mark.svg');
-    //    background-size: contain;
-    //    background-repeat: no-repeat;
-    //    position: absolute;
-    //    top: 10px;
-    //    left: 0px;
-    //    width: 30px;
-    //    height: 30px;
-    //    z-index: 1;
-    //}
+    &::before {
+        content: url("${quotationMark}");
+        position: absolute;
+        top: -14px;
+        left: 10px;
+        max-height: 30px;
+        z-index: 1;
+    }
 `
 
-const QuotationMarkAfter = styled.img`
-    position: absolute;
-    top: 80px;
-    right: 12px;
-    max-height: 30px;
-`
+// const QuotationMarkAfter = styled.img`
+//     position: absolute;
+//     top: 80px;
+//     right: 12px;
+//     max-height: 30px;
+// `
 
 const StyledCite = styled.cite`
     ${font({weight:500, Fmax: 24, Fmin:16})};
     border: 1px solid ${theme.colors.primaryOutline};
     border-top: none;
     padding: 16px;
+
+    &::before {
+        content: url("${quotationMark}");
+        position: absolute;
+        top: 80px;
+        right: 12px;
+        max-height: 30px;
+    }
 `
