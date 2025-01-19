005cf4ea82f3fac4445d57b7dd3a8c7fe26e403a
Kanin Gleb <gleb.kanin@gmail.com>
2025-01-20 00:40:54 +0300

    Footer media started

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -4,6 +4,7 @@ import {Container} from "../../components/Container.tsx";
 import {FlexWrapper} from "../../components/FlexWrapper.tsx";
 import {Logo} from "../../components/logo/Logo.tsx";
 import {SocialList} from "../../components/SocialList.tsx";
+import {font} from "../../styles/CommonFont.tsx";
 
 export const Footer = () => {
     return (
@@ -31,15 +32,14 @@ export const Footer = () => {
                 <small>© Copyright 2022. Made by Elias</small>
             </FooterContainer>
         </StyledFooter>
-    );
-};
+    )
+}
 
 const StyledFooter = styled.footer`
     text-align: center;
     margin: 145px 0 32px;
     border-top: 1px solid ${theme.colors.primaryOutline};
-
-
+    
     h3 {
         font-weight: 500;
         font-size: 24px;
@@ -49,7 +49,14 @@ const StyledFooter = styled.footer`
         font-size: 16px;
         color: #abb2bf;
     }
-`;
+    
+    @media ${theme.media.tablet} {
+        margin: 70px 0 16px;
+        small {
+            ${font({weight: 400, color: theme.colors.primaryLightText, Fmax: 16, Fmin: 14})};
+        }
+    }
+`
 
 const FooterContainer = styled(Container)`
 
@@ -57,9 +64,16 @@ const FooterContainer = styled(Container)`
 
 const BothSections = styled(FlexWrapper)`
     margin: 32px 0 48px;
+
+    @media ${theme.media.tablet} {
+        flex-wrap: wrap;
+        margin: 20px 0 30px;
+        gap: 30px;
+    }
 `
 
 const LeftSection = styled.div`
+    text-align: left;
     div {
         margin-bottom: 16px;
     }
