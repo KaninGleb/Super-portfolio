3826b8588c6687ed862e49a45ae51f12e02b2493
Kanin Gleb <gleb.kanin@gmail.com>
2025-01-21 22:27:52 +0300

    Container updated

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,9 +1,13 @@
 import styled from "styled-components";
+import {theme} from "../styles/Theme.tsx";
 
 export const Container = styled.div`
     max-width: 1024px;
     width: 100%;
     min-height: 100%;
     margin: 0 auto;
-    border: 1px solid red;
+
+    @media ${theme.media.width1044} {
+        padding: 0 16px;
+    }
 `
