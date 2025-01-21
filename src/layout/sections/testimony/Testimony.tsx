9eab806f473608665412413ef6053c6df24bab32
Kanin Gleb <gleb.kanin@gmail.com>
2025-01-21 17:33:08 +0300

    Testimony quotation marks fixed

--- src/layout/sections/testimony/Testimony.tsx
+++ src/layout/sections/testimony/Testimony.tsx
@@ -12,10 +12,8 @@ export const Testimony = () => {
             <Container>
                 <FlexWrapper justify="center">
                     <StyledFlexWrapper direction="column" align="flex-end">
-                        {/*<QuotationMarkBefore src={quotationMark} alt="Quotation mark before"/>*/}
                         <StyledQuote>With great power comes great electricity bill</StyledQuote>
                         <StyledCite>- Dr. Who</StyledCite>
-                        {/*<QuotationMarkAfter src={quotationMark} alt="Quotation mark after"/>*/}
                     </StyledFlexWrapper>
                 </FlexWrapper>
             </Container>
@@ -27,7 +25,7 @@ const StyledTestimony = styled.section`
     font-size: 24px;
     margin-bottom: 74px;
     position: relative;
-    //overflow: hidden;
+    overflow-x: clip;
     
     &::after {
         content: "";
@@ -56,19 +54,13 @@ const StyledFlexWrapper = styled(FlexWrapper)`
     position: relative;
 `
 
-// const QuotationMarkBefore = styled.img`
-//     position: absolute;
-//     top: -14px;
-//     left: 10px;
-//     max-height: 30px;
-// `
-
 const StyledQuote = styled.blockquote`
     ${font({weight:500, Fmax: 24, Fmin:16})};
     border: 1px solid ${theme.colors.primaryOutline};
     padding: 32px;
     max-width: 714px;
     width: 100%;
+    position: relative;
     
     &::before {
         content: "";
@@ -85,6 +77,20 @@ const StyledQuote = styled.blockquote`
         z-index: 1;
     }
     
+    &::after {
+        content: "";
+        background-image: url("${quotationMark}");
+        background-size: contain;
+        background-repeat: no-repeat;
+        position: absolute;
+        max-width: 42px;
+        width: 100%;
+        height: 100%;
+        bottom: -14px;
+        right: 12px;
+        max-height: 30px;
+    }
+    
     @media ${theme.media.tablet} {
         max-width: fit-content;
         padding: 26px;
@@ -96,32 +102,11 @@ const StyledQuote = styled.blockquote`
     }
 `
 
-// const QuotationMarkAfter = styled.img`
-//     position: absolute;
-//     top: 80px;
-//     right: 12px;
-//     max-height: 30px;
-// `
-
 const StyledCite = styled.cite`
     ${font({weight:500, Fmax: 24, Fmin:16})};
     border: 1px solid ${theme.colors.primaryOutline};
     border-top: none;
     padding: 16px;
-
-    &::before {
-        content: "";
-        background-image: url("${quotationMark}");
-        background-size: contain;
-        background-repeat: no-repeat;
-        position: absolute;
-        max-width: 42px;
-        width: 100%;
-        height: 100%;
-        top: 80px;
-        right: 12px;
-        max-height: 30px;
-    }
 
     @media ${theme.media.tablet} {
         padding: 14px;
