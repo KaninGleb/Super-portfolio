5ec07138ea744528f1c9ffe34c04e3a94fa7fb05
Kanin Gleb <gleb.kanin@gmail.com>
2025-01-25 19:27:07 +0300

    NotFoundPage404 - added and implemented

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import {ProjectsPage} from "./layout/projectsPage/ProjectsPage.tsx";
 import {AboutMePage} from "./layout/aboutMePage/AboutMePage.tsx";
 import {ContactsPage} from "./layout/contactsPage/ContactsPage.tsx";
 import {ScrollToTop} from "./common/ScrollToTop.tsx";
+import {NotFoundPage404} from "./layout/notFoundPage/NotFoundPage.tsx";
 
 function App() {
     return (
@@ -16,6 +17,7 @@ function App() {
                     <Route path="/projects" element={<ProjectsPage/>}/>
                     <Route path="/about-me" element={<AboutMePage/>}/>
                     <Route path="/contacts" element={<ContactsPage/>}/>
+                    <Route path="*" element={<NotFoundPage404/>} />
                 </Routes>
             </div>
         </Router>
