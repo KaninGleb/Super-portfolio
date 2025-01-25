6aa2db339ca8bd1d49f94f1f943fefafc6a8fb12
Kanin Gleb <gleb.kanin@gmail.com>
2025-01-26 02:38:22 +0300

    Skills - refactoring

--- src/layout/homePage/sections/skills/Skills.tsx
+++ src/layout/homePage/sections/skills/Skills.tsx
@@ -1,48 +1,40 @@
 import styled from "styled-components";
+import {theme} from "../../../../styles/Theme.tsx";
 import {Container} from "../../../../components/Container.tsx";
 import {FlexWrapper} from "../../../../components/FlexWrapper.tsx";
 import {SectionTitle} from "../../../../components/SectionTitle.tsx";
-import {sectionsData} from "../../../../data/appData.ts";
+import {sectionsData, skillsData} from "../../../../data/appData.ts";
 import {Skill} from "./skill/Skill.tsx";
 import {Photo} from "../../../../components/Photo.tsx";
 import groupOfImages from "../../../../assets/images/skills/skills-group.svg"
 import {imagePulseAnimation} from "../../../../animations/animations.ts";
-import {theme} from "../../../../styles/Theme.tsx";
 
 export const Skills = () => {
     return (
         <StyledSkills>
             <Container>
                 <FlexWrapper>
-                    <SectionTitle section={sectionsData.skills}/>
+                    <SectionTitle section={sectionsData.homePage.skills}/>
                 </FlexWrapper>
 
                 <StyledFlexWrapper gap='59px'>
                     <StyledPhoto src={groupOfImages} alt="Group of Images"/>
                         <SkillsWrapper direction='column' wrap='wrap-reverse' gap='16px'>
-                            <Skill order={5}
-                                    title='Languages'
-                                    description={['TypeScript', 'Lua', 'Python', 'JavaScript']}/>
-                            <Skill order={3}
-                                    title='Databases'
-                                    description={['SQLite', 'PostgreSQL', 'Mongo']}/>
-                            <Skill order={1}
-                                    title='Tools'
-                                    maxWidth='196px'
-                                    description={['VSCode', 'Neovim', 'Linux', 'Figma', 'XFCE', 'Arch', 'Git Font Awesome']}/>
-                            <Skill order={4}
-                                    title='Other'
-                                    description={['HTML', 'CSS', 'EJS', 'SCSS', 'REST', 'Jinja']}/>
-                            <Skill order={2}
-                                    title='Frameworks'
-                                    maxWidth='196px'
-                                    description={['React', 'Vue', 'Disnake', 'Discord.js', 'Flask', 'Express.js']}/>
+                            {skillsData.map((skill) => (
+                                <Skill
+                                    key={skill.title}
+                                    order={skill.order}
+                                    title={skill.title}
+                                    description={skill.description}
+                                    maxWidth={skill.maxWidth}
+                                />
+                            ))}
                         </SkillsWrapper>
                 </StyledFlexWrapper>
             </Container>
         </StyledSkills>
-    );
-};
+    )
+}
 
 const StyledSkills = styled.section`
     margin-bottom: 112px;
