48fb4f99dc4650128de29923c242ed8a956d5020
Kanin Gleb <gleb.kanin@gmail.com>
2025-01-26 03:55:51 +0300

    Media h1 supplemented

--- src/layout/homePage/media/Media.tsx
+++ src/layout/homePage/media/Media.tsx
@@ -7,12 +7,12 @@ export const Media = () => {
     return (
         <StyledMedia>
             <HeaderSEO>
-                Portfolio, Frontend Developer, UI/UX Designer, Web Development, Responsive Design, JavaScript, React, HTML, CSS, Bootstrap, Tailwind CSS,
+                Kanin Gleb, Gleb Kanin, KaninGleb, Portfolio, Frontend Developer, UI/UX Designer, Web Development, Responsive Design, JavaScript, React, HTML, CSS, Bootstrap, Tailwind CSS,
                 Figma, Adobe XD, User Experience, User Interface, Cross-Browser Compatibility, Version Control (Git), GitHub Pages Agile Methodologies,
                 RESTful APIs, Mobile-First Design, Performance Optimization, SEO Best Practices, Accessibility (a11y), Wireframing,
                 Prototyping, Graphic Design, Animation, CSS Preprocessors (Sass, Less), TypeScript, Frontend Frameworks, Code Reviews, Interview, Mock interview
 
-                Портфолио, Фронтенд-разработчик, UI/UX дизайнер, Веб-разработка, Адаптивный дизайн, JavaScript, React, HTML, CSS, Bootstrap, Tailwind CSS,
+                Канин Глеб, Глеб Канин, Портфолио, Фронтенд-разработчик, UI/UX дизайнер, Веб-разработка, Адаптивный дизайн, JavaScript, React, HTML, CSS, Bootstrap, Tailwind CSS,
                 Figma, Adobe XD, Пользовательский опыт, Пользовательский интерфейс, Кросс-браузерная совместимость, Контроль версий (Git),
                 Гибкие методологии, RESTful API, Мобильный дизайн, Оптимизация производительности, Лучшие практики SEO, Доступность (a11y), Гитхаб,
                 Проведение каркасных макетов, Прототипирование, Графический дизайн, Анимация, Препроцессоры CSS (Sass, Less), TypeScript,
