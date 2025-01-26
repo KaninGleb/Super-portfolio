b6c9b3d957aa147ffd0de13a1bb9ac1a1aa9d138
Kanin Gleb <gleb.kanin@gmail.com>
2025-01-26 11:08:36 +0300

    Logo - React-awesome-reveal animation added

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,4 +1,5 @@
 import styled, {css} from "styled-components";
+import {Fade} from "react-awesome-reveal";
 import {Icon} from "../icon/Icon.tsx";
 import {theme} from "../../styles/Theme.tsx";
 import {Link, useLocation} from "react-router-dom";
@@ -18,8 +19,12 @@ export const Logo = ({ isOpen = false }: LogoPropsType) => {
 
     return (
         <StyledLink to={"/"} isOpen={isOpen} onClick={handleClick}>
-            <Icon iconId='defaultLogo' width='16px' height='17px'/>
-            <span>Elias</span>
+            <FadeIconWrap direction={'left'} duration={400} delay={200}>
+                <Icon iconId='defaultLogo' width='16px' height='17px'/>
+            </FadeIconWrap>
+            <Fade direction={'down'} duration={400}>
+                <span>Elias</span>
+            </Fade>
         </StyledLink>
     )
 }
@@ -53,4 +58,8 @@ const StyledLink = styled(Link)<{ isOpen: boolean }>`
             left: 16px;
         }
     `}
+`
+
+const FadeIconWrap = styled(Fade)`
+    display: flex;
 `
