1668a2471d13bdceeca2e35c4d09cf9dd1e01121
Kanin Gleb <gleb.kanin@gmail.com>
2025-01-26 10:54:06 +0300

    SkillsAboutMe - React-awesome-reveal animation added

--- src/layout/aboutMePage/SkillsAboutMe.tsx
+++ src/layout/aboutMePage/SkillsAboutMe.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import {Fade} from "react-awesome-reveal";
 import {theme} from "../../styles/Theme.tsx";
 import {Container} from "../../components/Container.tsx";
 import {FlexWrapper} from "../../components/FlexWrapper.tsx";
@@ -13,12 +14,13 @@ export const SkillsAboutMe = () => {
                 <SectionTitle section={sectionsData.aboutMePage.skills}/>
                 <SkillsWrapper gap='16px'>
                     {skillsData.map((skill, index) => (
-                        <Skill
-                            key={index}
-                            title={skill.title}
-                            description={skill.description}
-                            maxWidth={skill.maxWidth}
-                        />
+                        <FadeWrapper key={index} delay={index * 200} direction={'up'} duration={600}>
+                            <Skill
+                                title={skill.title}
+                                description={skill.description}
+                                maxWidth={skill.maxWidth}
+                            />
+                        </FadeWrapper>
                     ))}
                 </SkillsWrapper>
             </Container>
@@ -36,11 +38,20 @@ const StyledSkillsAboutMe = styled.section`
 
 const SkillsWrapper = styled(FlexWrapper)`
     margin-top: 48px;
-    
+
     @media ${theme.media.tablet} {
         flex-direction: row;
         flex-wrap: wrap;
         margin-top: 25px;
         max-height: unset;
+    }
+`
+
+const FadeWrapper = styled(Fade)`
+    display: flex;
+    flex-grow: 1;
+    
+    @media (min-width: 1024px) {
+        flex-grow: unset;
     }
 `
