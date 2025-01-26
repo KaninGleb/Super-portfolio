6ee719df5e35f6bcec8d9bef98cdad87365add4f
Kanin Gleb <gleb.kanin@gmail.com>
2025-01-26 11:00:41 +0300

    ContactsForContactsPage - React-awesome-reveal animation added

--- src/layout/contactsPage/ContactsForContactsPage.tsx
+++ src/layout/contactsPage/ContactsForContactsPage.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import {Fade} from "react-awesome-reveal";
 import {Container} from "../../components/Container.tsx";
 import {FlexWrapper} from "../../components/FlexWrapper.tsx";
 import {Paragraph} from "../../components/Paragraph.tsx";
@@ -7,7 +8,6 @@ import {theme} from "../../styles/Theme.tsx";
 import {textData} from "../../data/appData.ts";
 import bgLeft from "../../assets/images/background/contacts-page/contacts-page-bg-pseudo-left-short.svg";
 import bgRight from "../../assets/images/background/contacts-page/contacts-page-bg-pseudo-right-short.svg";
-
 import {imagePulseAnimation} from "../../animations/animations.ts";
 
 
@@ -18,25 +18,32 @@ export const ContactsForContactsPage = () => {
                 <ContentFlexWrapper direction='column' gap='45px'>
                     <StyledFlexWrapper justify='space-between'>
                         <LeftSection direction='column' gap='20px'>
-                            <Paragraph children={textData.contacts.paragraph} fontWeight='500' lineHeight={'1.3'}></Paragraph>
+                            <Fade direction={'up'}>
+                                <Paragraph children={textData.contacts.paragraph} fontWeight='500' lineHeight={'1.3'}></Paragraph>
+                            </Fade>
                         </LeftSection>
-
                         <RightSection gap='10px'>
-                            <SupportWrapper direction='column' gap='8px'>
-                                <Title>Support me here</Title>
-                                <Description>4149500120690030</Description>
-                            </SupportWrapper>
-                            <MessageWrapper direction='column' gap='16px'>
-                                <Title>Message me here</Title>
-                                <SocialList displayFlex={true}
-                                            direction={'column'}
-                                            gap='8px'
-                                            iconIds={['discord', 'email']}
-                                            showIcon={true}
-                                            iconsTitles={['!Elias#3519', 'elias@elias-dev.ml']}
-                                            gapBetween={'5px'}
-                                />
-                            </MessageWrapper>
+                            <Fade direction={'right'} cascade damping={.1}>
+                                <SupportWrapper direction='column' gap='8px'>
+                                    <Fade direction={'up'} cascade damping={.2}>
+                                        <Title>Support me here</Title>
+                                        <Description>4149500120690030</Description>
+                                    </Fade>
+                                </SupportWrapper>
+                                <MessageWrapper direction='column' gap='16px'>
+                                    <Fade direction={'up'}>
+                                        <Title>Message me here</Title>
+                                    </Fade>
+                                    <SocialList displayFlex={true}
+                                                direction={'column'}
+                                                gap='8px'
+                                                iconIds={['discord', 'email']}
+                                                showIcon={true}
+                                                iconsTitles={['!Elias#3519', 'elias@elias-dev.ml']}
+                                                gapBetween={'5px'}
+                                    />
+                                </MessageWrapper>
+                            </Fade>
                         </RightSection>
                     </StyledFlexWrapper>
                 </ContentFlexWrapper>
