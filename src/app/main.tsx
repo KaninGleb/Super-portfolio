310a50dd18cbc853b0cd3a188a0f96968eaea277
Kanin Gleb <gleb.kanin@gmail.com>
2025-05-11 19:10:13 +0300

    Refactoring - main.tsx import paths updated

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import { GlobalStyles } from '../styles/Global.styles.tsx'
+import { GlobalStyles } from '@/styles'
 import { SpeedInsights } from '@vercel/speed-insights/react'
 
 createRoot(document.getElementById('root')!).render(
