1ae3aaec11069dec5c9f38d2537932e373e6c930
Kanin Gleb <gleb.kanin@gmail.com>
2025-05-11 18:36:57 +0300

    Refactoring - index.ts supplemented with ContactsPage components

--- src/components/index.ts
+++ src/components/index.ts
@@ -6,3 +6,9 @@ export {SkillsAboutMe} from './AboutMePage/SkillsAboutMe'
 export {MyFunFacts} from './AboutMePage/MyFunFacts/MyFunFacts'
 export {Fact} from './AboutMePage/MyFunFacts/Fact/Fact'
 
+// Contacts Page
+export {ContactsPage} from './ContactsPage/ContactsPage'
+
+export {AllMedia} from './ContactsPage/AllMedia/AllMedia'
+export {ContactsForContactsPage} from './ContactsPage/ContactsForContactsPage/ContactsForContactsPage'
+export {CopyableText} from './ContactsPage/ContactsForContactsPage/CopyableText/CopyableText'
