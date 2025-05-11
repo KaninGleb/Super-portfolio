76f41a84754001487bfec8f72868ad5a649e6659
Kanin Gleb <gleb.kanin@gmail.com>
2025-05-11 18:42:30 +0300

    Refactoring - index.ts supplemented with HomePage components

--- src/components/index.ts
+++ src/components/index.ts
@@ -12,3 +12,30 @@ export {ContactsPage} from './ContactsPage/ContactsPage'
 export {AllMedia} from './ContactsPage/AllMedia/AllMedia'
 export {ContactsForContactsPage} from './ContactsPage/ContactsForContactsPage/ContactsForContactsPage'
 export {CopyableText} from './ContactsPage/ContactsForContactsPage/CopyableText/CopyableText'
+
+// Home Page
+export {HomePage} from './HomePage/HomePage'
+
+// Home Page - Footer
+export {Footer} from './HomePage/Footer/Footer'
+
+// Home Page - Header
+export {Header} from './HomePage/Header/Header'
+export {HeaderMenu} from './HomePage/Header/HeaderMenu/HeaderMenu'
+export {MobileMenu} from './HomePage/Header/MobileMenu/MobileMenu'
+
+// Home Page - Media
+export {Media} from './HomePage/Media/Media'
+
+// Home Page - Sections
+export {AboutMe} from './HomePage/Sections/AboutMe/AboutMe'
+export {Contacts} from './HomePage/Sections/Contacts/Contacts'
+export {Main} from './HomePage/Sections/Main/Main'
+
+export {Projects} from './HomePage/Sections/Projects/Projects'
+export {Project} from './HomePage/Sections/Projects/Project/Project'
+
+export {Skills} from './HomePage/Sections/Skills/Skills'
+export {Skill} from './HomePage/Sections/Skills/Skill/Skill'
+
+export {Testimony} from './HomePage/Sections/Testimony/Testimony'
