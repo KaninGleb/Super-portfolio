a462ac5b3a150ba640f05272e854c730361c3d23
Kanin Gleb <gleb.kanin@gmail.com>
2025-05-11 19:06:14 +0300

    Refactoring - assets / index.ts supplemented with images imports

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,4 +3,10 @@ import logo from '@/assets/images/sections-pseudo/main-pseudo/logo-outline.svg'
 import dotsForMain from '@/assets/images/sections-pseudo/main-pseudo/dots-main-photo.svg'
 import CV from '@/assets/cv/Kanin_Gleb_Front-end_developer_CV.pdf'
 
-export { photo, logo, dotsForMain, CV }
+import portfolio from '@/assets/images/section-images/projects/project-1.svg'
+import kotikBot from '@/assets/images/section-images/projects/project-2.svg'
+import kahoot from '@/assets/images/section-images/projects/project-3.svg'
+import protectX from '@/assets/images/section-images/projects/project-4.svg'
+import chertNodes from '@/assets/images/section-images/projects/project-5.svg'
+
+export { photo, logo, dotsForMain, CV, portfolio, kotikBot, kahoot, protectX, chertNodes }
