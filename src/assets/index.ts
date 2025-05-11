db0103bf8be61f340f1d73835ebf1a6b70396c21
Kanin Gleb <gleb.kanin@gmail.com>
2025-05-11 18:00:11 +0300

    Refactoring - index.ts added to assets folder

---
+++ src/assets/index.ts
@@ -0,0 +1,6 @@
+import photo from '@/assets/images/section-images/main/main-my-photo.png'
+import logo from '@/assets/images/sections-pseudo/main-pseudo/logo-outline.svg'
+import dotsForMain from '@/assets/images/sections-pseudo/main-pseudo/dots-main-photo.svg'
+import CV from '@/assets/cv/Kanin_Gleb_Front-end_developer_CV.pdf'
+
+export {photo, logo, dotsForMain, CV}
