b2ecfe80d4fdb3830745c078e5b02d2c1b92117a
Kanin Gleb <gleb.kanin@gmail.com>
2025-05-11 18:33:12 +0300

    Refactoring - index.ts supplemented with AboutMePage components

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+// About Me page
+export {AboutMeForPage} from './AboutMePage/AboutMeForPage'
+export {AboutMePage} from './AboutMePage/AboutMePage'
+export {SkillsAboutMe} from './AboutMePage/SkillsAboutMe'
+
+export {MyFunFacts} from './AboutMePage/MyFunFacts/MyFunFacts'
+export {Fact} from './AboutMePage/MyFunFacts/Fact/Fact'
+
