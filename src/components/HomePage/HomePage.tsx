8109350430992075d6a6c8c51fa0bd4b7e852800
Kanin Gleb <gleb.kanin@gmail.com>
2025-05-16 22:57:04 +0300

    Refactoring - HomePage import paths updated

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,14 +1,5 @@
-import { ScrollFillBar } from '../../common/ScrollFillBar.tsx'
-import { Media } from './Media/Media.tsx'
-import { RouterHeader } from '../../common/RouterHeader/RouterHeader.tsx'
-import { Main } from './Sections/Main/Main.tsx'
-import { Testimony } from './Sections/Testimony/Testimony.tsx'
-import { Projects } from './Sections/Projects/Projects.tsx'
-import { Skills } from './Sections/Skills/Skills.tsx'
-import { AboutMe } from './Sections/AboutMe/AboutMe.tsx'
-import { Contacts } from './Sections/Contacts/Contacts.tsx'
-import { Footer } from './Footer/Footer.tsx'
-import { GoTopBtn } from '../../common/GoTopBtn.tsx'
+import { ScrollFillBar, RouterHeader, GoTopBtn } from '@/common'
+import { Media, Main, Testimony, Projects, Skills, AboutMe, Contacts, Footer } from '@/components'
 
 export const HomePage = () => {
   return (
